241244f0ee19fc61bda7df8ec8a4631aaaf4d76d
Daniel Park <ekklesia89@gmail.com>
2020-06-20 14:21:54 +0900

    stack all methods updated

--- javascript/stack.js
+++ javascript/stack.js
@@ -7,46 +7,85 @@ class Stack {
 
   // methods
   push(data) {
-    let lastKey;
-    for (let key in this) {
-      lastKey = key;
-    }
-
-    if (!lastKey) {
-      this["0"] = data;
-    } else {
-      this[Number(lastKey) + 1] = data;
-    }
-
+    let lastKey = this.length();
+    this[lastKey.toString()] = data;
     return data;
   }
 
-  pop() {}
+  pop() {
+    if (!this.isEmpty()) {
+      let lastKey = (this.length() - 1).toString();
+      let lastValue = this[lastKey];
+      delete this[lastKey];
+      return lastValue;
+    } else {
+      return;
+    }
+  }
 
-  peek() {}
+  peek() {
+    if (!this.isEmpty()) {
+      let lastKey = (this.length() - 1).toString();
+      let lastValue = this[lastKey];
+      return lastValue;
+    } else {
+      return;
+    }
+  }
 
-  clear() {}
+  clear() {
+    if (this.isEmpty()) {
+      return "nothing to clear!";
+    } else {
+      for (let key in this) {
+        delete this[key];
+      }
+      return "clear done!";
+    }
+  }
 
-  isEmpty() {}
+  isEmpty() {
+    return this.length() ? false : true;
+  }
 
   length() {
     let leng = 0;
     for (let key in this) {
-      leng = Number(key);
+      if (key) {
+        leng++;
+      } else {
+        return 0;
+      }
     }
 
     return leng;
   }
 }
 
-// test
+// stack test
 const stack = new Stack();
 
 console.log("empty stack : ", stack);
-console.group("actions");
-console.log("push return : ", stack.push(1));
-console.log("push return : ", stack.push(2));
-console.log("push return : ", stack.push(3));
+console.log("length : ", stack.length());
+console.log("clear return : ", stack.clear());
+
+console.log("");
+console.group("methods test ===============");
+console.log("isEmpty return : ", stack.isEmpty());
+console.log("first push return : ", stack.push("a"));
+console.log("second push return : ", stack.push("b"));
+console.log("third push return : ", stack.push("c"));
+console.log("length : ", stack.length());
+console.log("pop return : ", stack.pop());
+console.log("peek return : ", stack.peek());
+console.log("isEmpty return : ", stack.isEmpty());
+console.log("length : ", stack.length());
+console.log("clear return : ", stack.clear());
+console.log("isEmpty return : ", stack.isEmpty());
 console.groupEnd();
+console.log("=============================");
+console.log("");
+
 console.log("after action : ", stack);
 console.log("length : ", stack.length());
+console.log("clear return : ", stack.clear());
